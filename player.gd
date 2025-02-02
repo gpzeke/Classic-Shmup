56f26e54e6663ad7cae44fbe2d03f3bbf9cf1f9f
Alex Shelton <65458253+gpzeke@users.noreply.github.com>
2025-02-02 15:50:24 -0600

    completed bullet and added shoot functionality

--- player.gd
+++ player.gd
@@ -1,12 +1,28 @@
 extends Area2D
 
 @onready var screensize = get_viewport_rect().size
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	start()
+	
+func start() -> void:
+	position = Vector2(screensize.x / 2, screensize.y - 64)
+	$GunCooldown.wait_time = cooldown
+	
+func shoot() -> void:
+	if not can_shoot:
+		return
+	can_shoot = false
+	$GunCooldown.start()
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start(position + Vector2(0, -8))
 
 @export var speed = 150
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@export var cooldown = 0.25
+@export var bullet_scene : PackedScene
+var can_shoot = true
+
 func _process(delta: float) -> void:
 	var input = Input.get_vector("left", "right", "up", "down")
 	if input.x > 0:
@@ -20,3 +36,10 @@ func _process(delta: float) -> void:
 		$Ship/Boosters.animation = "forward"
 	position += input * speed * delta
 	position = position.clamp(Vector2(8, 8), screensize - Vector2(8, 8))
+	
+	if Input.is_action_pressed("shoot"):
+		shoot()
+
+
+func _on_gun_cooldown_timeout() -> void:
+	can_shoot = true
