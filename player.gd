3e8e3c0975c73c66185ce5dbffa5132d12630d13
gpzeke <65458253+gpzeke@users.noreply.github.com>
2025-02-02 15:08:02 -0600

    Designing the player scene

---
+++ player.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@onready var screensize = get_viewport_rect().size
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+@export var speed = 150
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var input = Input.get_vector("left", "right", "up", "down")
+	if input.x > 0:
+		$Ship.frame = 2
+		$Ship/Boosters.animation = "right"
+	elif input.x < 0:
+		$Ship.frame = 0
+		$Ship/Boosters.animation = "left"
+	else:
+		$Ship.frame = 1
+		$Ship/Boosters.animation = "forward"
+	position += input * speed * delta
+	position = position.clamp(Vector2(8, 8), screensize - Vector2(8, 8))
